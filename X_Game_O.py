f9d7135e1312b08a621c316ce6f053a19b56104d
Timur <polyanskiy.tima@gmail.com>
2024-07-11 22:36:51 +0300

    Game tic tac toe with Ilya

---
+++ X_Game_O.py
@@ -0,0 +1,154 @@
+from random import randint
+
+class Game:
+    matrix = [[0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0]]
+
+    def __init__(self, first_player, second_player):
+        self.first_player = first_player
+        self.second_player = second_player
+
+    def chek_is_winner(self, value):
+        not_matrix = self.matrix[0]
+        not_matrix2 = self.matrix[1]
+        not_matrix3 = self.matrix[2]
+
+        if not_matrix[0] == value and not_matrix[1] == value and not_matrix[2] == value:
+             return True
+        if not_matrix2[0] == value and not_matrix2[1] == value and not_matrix2[2] == value:
+            return True
+        if not_matrix3[0] == value and not_matrix3[1] == value and not_matrix3[2] == value:
+            return True
+        if not_matrix[0] == value and not_matrix2[0] == value and not_matrix3[0] == value:
+            return True
+        if not_matrix[1] == value and not_matrix2[1] == value and not_matrix3[1] == value:
+            return True
+        if not_matrix[2] == value and not_matrix2[2] == value and not_matrix3[2] == value:
+            return True
+        if not_matrix[0] == value and not_matrix2[1] == value and not_matrix3[2] == value:
+            return True
+        if not_matrix[2] == value and not_matrix2[2] == value and not_matrix3[0] == value:
+            return True
+        return False
+
+    def start_game(self, Player_Winner1=False, Player_Winner2=False):
+        random_move_player = randint(0, 1)
+        count = 0
+        while not Player_Winner1 or not Player_Winner2 or count > 7:
+            if random_move_player == 1:
+                result_1 = self.first_player.turn(self.matrix, 1)
+                count += 1
+                random_move_player -= 1
+                print(self.matrix)
+                Player_Winner1 = self.chek_is_winner(1)
+            else:
+                result_2 = self.second_player.turn(self.matrix, -1)
+                count += 1
+                random_move_player += 1
+                print(self.matrix)
+                Player_Winner2 = self.chek_is_winner(-1)
+
+        if Player_Winner1 == True:
+            return f'{Победил} {крестик}'
+        elif Player_Winner2 == True:
+            return f'{Победил} {нолик}'
+        else:
+            return f'{Ничья}'
+
+class Player:
+
+    # matrix - двумерный массив, поле игры
+    # value - -1 - играешь за 0, 1 - играешь за X
+    def turn(self, matrix, value):
+        raise NameError("Походу неправильно указали имя в дочернем классе, советую перепроверить.")
+
+class TimurPlayer(Player):
+    def turn(self, matrix, value):
+        obj_matrix = matrix[0]
+        obj_matrix2 = matrix[1]
+        obj_matrix3 = matrix[2]
+
+        if obj_matrix[0] == value and obj_matrix[1] == value:
+            obj_matrix[2] = value
+            return True
+        elif obj_matrix[2] == value  and obj_matrix[1] == value:
+            obj_matrix[0] = value
+            return True
+        elif obj_matrix2[0] == value and obj_matrix2[1] == value:
+            obj_matrix2[2] = value
+            return True
+        elif obj_matrix2[2] == value and obj_matrix2[1] == value:
+            obj_matrix2[0] = value
+            return True
+        elif obj_matrix3[0] == value and obj_matrix3[1] == value:
+            obj_matrix3[2] = value
+            return True
+        elif obj_matrix3[2] == value and obj_matrix3[1] == value:
+            obj_matrix3[0] = value
+            return True
+
+        elif obj_matrix[0] == value and obj_matrix2[0] == value:
+            obj_matrix3[0] == value
+            return True
+        elif obj_matrix3[0] == value and obj_matrix2[0] == value:
+            obj_matrix[0] == value
+            return True
+        elif obj_matrix[1] == value and obj_matrix2[1] == value:
+            obj_matrix3[1] == value
+            return True
+        elif obj_matrix3[1] == value and obj_matrix2[1] == value:
+            obj_matrix[1] == value
+            return True
+        elif obj_matrix[2] == value and obj_matrix2[2] == value:
+            obj_matrix3[2] == value
+            return True
+        elif obj_matrix3[2] == value and obj_matrix2[2] == value:
+            obj_matrix[2] == value
+            return True
+
+        elif obj_matrix[0] == value and obj_matrix2[1] == value:
+            obj_matrix3[2] = value
+            return True
+        elif obj_matrix3[2] == value and obj_matrix2[1] == value:
+            obj_matrix[0] = value
+            return True
+        elif obj_matrix[2] == value and obj_matrix2[1] == value:
+            obj_matrix3[0] = value
+            return True
+        elif obj_matrix3[0] == value and obj_matrix2[1] == value:
+            obj_matrix[2] = value
+            return True
+        else:
+            if obj_matrix2[1] != -1:
+                obj_matrix2[1] = value
+                return True
+            if obj_matrix3[0] != -1:
+                obj_matrix3[0] = value
+                return True
+            if obj_matrix[0] != -1:
+                obj_matrix[0] = value
+                return Tru
+            if obj_matrix2[0] != -1:
+                obj_matrix2[0] = value
+                return True
+            if obj_matrix[1] != -1:
+                obj_matrix[1] = value
+                return True
+            if obj_matrix[2] != -1:
+                obj_matrix[2] = value
+                return True
+            if obj_matrix2[2] != -1:
+                obj_matrix2[2] = value
+                return True
+            if obj_matrix3[1] != -1:
+                obj_matrix3[1] = value
+                return True
+            if obj_matrix3[2] != -1:
+                obj_matrix3[2] = value
+                return True
+            if obj_matrix2[2] != -1:
+                obj_matrix2[2] = value
+                return True
+game = Game(TimurPlayer(), IlyaPlayer())
+game.start_game()
